465a776c8cfd70e3c22581b6f0735f8a9c7aca4b
859taisoma <phamf574@gmail.com>
2018-09-15 23:35:26 +0700

    Update some modifications

--- app/src/test/java/com/example/phamf/calculator/ExampleUnitTest.kt
+++ app/src/test/java/com/example/phamf/calculator/ExampleUnitTest.kt
@@ -1,17 +1,16 @@
 package com.example.phamf.calculator
 
-import org.junit.Test
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
-import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun test_process_a_hard_math_case () {
+        var case = "(30*2)/5 + 15^2*3 - 6*2^2 + ((8*8)^2) - 9^2/3 * (2^2*(3+2^2)*5)^2"
+        var case2 = "20*3 - 30*2 - 3^2"
+        assertEquals("-524441", processValueOf(case.removeAllSpace().processMinus()))
+        assertEquals("-9", processValueOf(case2.removeAllSpace().processMinus()))
     }
 }
